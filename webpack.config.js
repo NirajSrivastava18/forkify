06ba087c4d9a9384455b53057eacec65383eee05
Niraj Srivastava <nirajsrivastava2001@gmail.com>
2020-05-14 00:37:21 +0530

    forkify

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+
+module.exports ={  
+    entry: ['./src/js/index.js'],
+    output: {
+        path: path.resolve(__dirname,'dist'), 
+        filename: 'js/bundle.js'
+    },
+    mode :"development",
+    devServer: {
+        contentBase : './dist'
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename:'index.html',
+            template:'./src/index.html'
+        })
+    ],
+        module:{
+            rules:[
+             {
+                test: /\.js$/,
+                exclude : /node_module/,
+                use:{
+                    loader: 'babel-loader'
+                }
+             }
+
+            ]
+        }
+
+};
